69d64bcbe8a2aa64eac984192a4f17d086500422
jochoi <chousemath@gmail.com>
2020-07-17 21:05:40 +0900

    Update

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, ScrollView, SafeAreaView, Text, View } from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, ScrollView, SafeAreaView, Text, View } from 'react-native';
+import CardFlip from 'react-native-card-flip';
 import * as Font from 'expo-font';
 import * as _ from 'lodash';
 import * as INTER from '@expo-google-fonts/inter';
 import * as ROBOTO from '@expo-google-fonts/roboto';
+import * as PTSANS from '@expo-google-fonts/pt-sans';
+import * as RALEWAY from '@expo-google-fonts/raleway';
 
+let card;
 const fonts = [
+    [
+        'Raleway_100Thin',
+        'Raleway_100Thin_Italic',
+        'Raleway_200ExtraLight',
+        'Raleway_200ExtraLight_Italic',
+        'Raleway_300Light',
+        'Raleway_300Light_Italic',
+        'Raleway_400Regular',
+        'Raleway_400Regular_Italic',
+        'Raleway_500Medium',
+        'Raleway_500Medium_Italic',
+        'Raleway_600SemiBold',
+        'Raleway_600SemiBold_Italic',
+        'Raleway_700Bold',
+        'Raleway_700Bold_Italic',
+        'Raleway_800ExtraBold',
+        'Raleway_800ExtraBold_Italic',
+        'Raleway_900Black',
+        'Raleway_900Black_Italic',
+    ],
+    [
+        'PTSans_400Regular',
+        'PTSans_400Regular_Italic',
+        'PTSans_700Bold',
+        'PTSans_700Bold_Italic',
+    ],
     [
         'Roboto_100Thin',
         'Roboto_100Thin_Italic',
@@ -50,9 +80,52 @@ const quote = [
     "You have no idea what I’m talking about, I’m sure.",
     "But don’t worry.",
     "You will someday.",
+    "I had always heard your entire life flashes in front of your eyes the second before you die.",
+    "First of all, that one second isn’t a second at all.",
+    "It stretches on forever, like an ocean of time.",
+    "For me, it was lying on my back at Boy Scout Camp, watching falling stars.",
+    "And yellow leaves from the maple trees that lined our street.",
+    "Or my grandmother’s hands, and the way her skin seemed like paper.",
+    "And the first time I saw my cousin Tony’s brand new Firebird.",
+    "And Janie, and Janie.",
+    "And Carolyn.",
+    "I guess I could be pretty pissed off about what happened to me, but it’s hard to stay mad when there’s so much beauty in the world.",
+    "Sometimes I feel like I’m seeing it all at once, and it’s too much.",
+    "My heart fills up like a balloon that’s about to burst.",
+    "And then I remember to relax, and stop trying to hold on to it, and then it flows through me like rain, and I can’t feel anything but gratitude for every single moment of my stupid little life.",
+    "You have no idea what I’m talking about, I’m sure.",
+    "But don’t worry.",
+    "You will someday.",
 ];
 export default function App() {
     const [text, setText] = useState([]);
+    const [font, setFont] = useState('');
+    let [loadedRaleway, errorRaleway] = RALEWAY.useFonts({
+        Raleway_100Thin: RALEWAY.Raleway_100Thin,
+        Raleway_100Thin_Italic: RALEWAY.Raleway_100Thin_Italic,
+        Raleway_200ExtraLight: RALEWAY.Raleway_200ExtraLight,
+        Raleway_200ExtraLight_Italic: RALEWAY.Raleway_200ExtraLight_Italic,
+        Raleway_300Light: RALEWAY.Raleway_300Light,
+        Raleway_300Light_Italic: RALEWAY.Raleway_300Light_Italic,
+        Raleway_400Regular: RALEWAY.Raleway_400Regular,
+        Raleway_400Regular_Italic: RALEWAY.Raleway_400Regular_Italic,
+        Raleway_500Medium: RALEWAY.Raleway_500Medium,
+        Raleway_500Medium_Italic: RALEWAY.Raleway_500Medium_Italic,
+        Raleway_600SemiBold: RALEWAY.Raleway_600SemiBold,
+        Raleway_600SemiBold_Italic: RALEWAY.Raleway_600SemiBold_Italic,
+        Raleway_700Bold: RALEWAY.Raleway_700Bold,
+        Raleway_700Bold_Italic: RALEWAY.Raleway_700Bold_Italic,
+        Raleway_800ExtraBold: RALEWAY.Raleway_800ExtraBold,
+        Raleway_800ExtraBold_Italic: RALEWAY.Raleway_800ExtraBold_Italic,
+        Raleway_900Black: RALEWAY.Raleway_900Black,
+        Raleway_900Black_Italic: RALEWAY.Raleway_900Black_Italic,
+    });
+    let [loadedPtsans, errorPtsans] = PTSANS.useFonts({
+        PTSans_400Regular: PTSANS.PTSans_400Regular,
+        PTSans_400Regular_Italic: PTSANS.PTSans_400Regular_Italic,
+        PTSans_700Bold: PTSANS.PTSans_700Bold,
+        PTSans_700Bold_Italic: PTSANS.PTSans_700Bold_Italic,
+    });
     let [loadedInter, errorInter] = INTER.useFonts({
         Inter_100Thin: INTER.Inter_100Thin,
         Inter_200ExtraLight: INTER.Inter_200ExtraLight,
@@ -78,7 +151,7 @@ export default function App() {
         Roboto_900Black: ROBOTO.Roboto_900Black,
         Roboto_900Black_Italic: ROBOTO.Roboto_900Black_Italic,
     });
-    useEffect(() => {
+    const resetList = () => {
         const _text = [];
         const _font = fonts[Math.floor(Math.random() * fonts.length)];
         let _quote = _.shuffle(quote);
@@ -86,6 +159,10 @@ export default function App() {
         _quote = _.shuffle(quote);
         for (let i = 0; i < (40 - _font.length); i++) _text.push({text: _quote[Math.floor(Math.random() * _quote.length)].slice(0, 30), font: _font[i],});
         setText(_text);
+        setFont(_font[0]);
+    };
+    useEffect(() => {
+        resetList();
     }, []);
     const renderText = (t, i) => {
         return (
@@ -101,14 +178,34 @@ export default function App() {
                 key={`text-${i}`}>{t.text}</Text>
         );
     };
+    const handleShowFont = () => {
+        card.flip();
+    };
+    const handleShowList = () => {
+        setTimeout(() => {
+            resetList();
+        }, 200);
+        card.flip();
+    };
   return (
       <View style={{flex: 1}}>
           <SafeAreaView style={styles.container}>
-              <ScrollView
-                  contentContainerStyle={styles.contentContainer}
-                  >
-                  {loadedInter && loadedRoboto && text.map(renderText)}
-              </ScrollView>
+              <CardFlip style={styles.cardContainer} ref={ref => card = ref} >
+                      <ScrollView
+                              showsVerticalScrollIndicator={false}
+                              contentContainerStyle={styles.contentContainer}>
+                  <TouchableWithoutFeedback onPress={handleShowFont}>
+                          <View style={{flex: 1}}>
+                              {loadedRaleway && loadedPtsans && loadedInter && loadedRoboto && text.map(renderText)}
+                              </View>
+                  </TouchableWithoutFeedback>
+                      </ScrollView>
+                  <TouchableWithoutFeedback onPress={handleShowList}>
+                      <View style={{flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', fontFamily: font,}}>
+                          <Text style={{fontSize: 32,}}>{font.split('_')[0]}</Text>
+                          </View>
+                  </TouchableWithoutFeedback>
+              </CardFlip>
               <StatusBar style="auto" />
       </SafeAreaView>
       </View>
@@ -116,13 +213,13 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+    },
   container: {
     flex: 1,
     backgroundColor: '#fff',
       paddingLeft: 32,
   },
-    contentContainer: {
-        paddingVertical: 16,
-        paddingHorizontal: 16,
-    }
+    contentContainer: {paddingHorizontal: 32},
 });
